a275b695214009b72a7bce8e8dd6270ab074c459
Olga <ol.gure@gmail.com>
2018-02-26 17:47:03 +0300

    docs: update docstrings

--- tokenizer.py
+++ tokenizer.py
@@ -21,7 +21,8 @@ class SpacyTokenizer:
     """
     def __init__(self, disable=None, stopwords=None):
         """
-        :param disable: pipeline processors to omit
+        :param disable: pipeline processors to omit; if nothing should be disabled,
+         pass an empty list
         :param stopwords: a set of words to skip
         """
         if disable is None:
@@ -39,7 +40,7 @@ class SpacyTokenizer:
         (1, 2), for bigrams only should be set to (2, 2)
         :param batch_size: the number of documents to process at once;
         improves the spacy 'pipe' performance; shouldn't be too small
-        :param n_threads: number of threads for parallel computing; doesn't work good
+        :param n_threads: a number of threads for parallel computing; doesn't work good
          on a standard Python
         :return: a single processed doc generator
         """
@@ -59,7 +60,7 @@ class SpacyTokenizer:
         (1, 2), for bigrams only should be set to (2, 2)
         :param batch_size: the number of documents to process at once;
         improves the spacy 'pipe' performance; shouldn't be too small
-        :param n_threads: number of threads for parallel computing; doesn't work good
+        :param n_threads: a number of threads for parallel computing; doesn't work good
          on a standard Python
         :return: a single processed doc generator
         """
@@ -71,7 +72,13 @@ class SpacyTokenizer:
             processed_doc = self.ngramize(lemmas, ngram_range=ngram_range)
             yield from processed_doc
 
-    def ngramize(self, items, ngram_range=(1, 1)):
+    def ngramize(self, items: List[str], ngram_range=(1, 1)):
+        """
+        :param items: list of tokens, lemmas or other strings to form ngrams
+        :param ngram_range: range for producing ngrams, ex. for unigrams + bigrams should be set to
+        (1, 2), for bigrams only should be set to (2, 2)
+        :return:
+        """
         filtered = list(
             filter(lambda x: x.isalpha() and x not in self.stopwords, items))
 
@@ -86,13 +93,13 @@ class SpacyTokenizer:
 
 
 # Test
-data = ['His paintings brought him both critical and commercial success,'
-        ' which enabled. him to set up his own. professional portrait studio'
-        ' in Chelsea, south-west London.', ' After the Great War finished, he met'
-        ' and fell in love with Katherine Gardiner, she immediately became his muse and features'
-        ' in many key work from the period. The couple married in 1921.']
-tok = SpacyTokenizer()
-items = tok.lemmatize(data)
-print(*list(i for i in items))
-items = tok.tokenize(data)
-print(*list(i for i in items))
+# data = ['His paintings brought him both critical and commercial success,'
+#         ' which enabled. him to set up his own. professional portrait studio'
+#         ' in Chelsea, south-west London.', ' After the Great War finished, he met'
+#         ' and fell in love with Katherine Gardiner, she immediately became his muse and features'
+#         ' in many key work from the period. The couple married in 1921.']
+# tok = SpacyTokenizer()
+# items = tok.lemmatize(data)
+# print(*list(i for i in items))
+# items = tok.tokenize(data)
+# print(*list(i for i in items))
