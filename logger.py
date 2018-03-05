83b84b0a32b51b24398a5f2a2c5e704f15f7ff1c
Olga <ol.gure@gmail.com>
2018-03-05 11:23:23 +0300

    refactor: outline logging

---
+++ logger.py
@@ -0,0 +1,8 @@
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+fmt = logging.Formatter('%(asctime)s: [ %(message)s ]', '%m/%d/%Y %I:%M:%S %p')
+console = logging.StreamHandler()
+console.setFormatter(fmt)
+logger.addHandler(console)
